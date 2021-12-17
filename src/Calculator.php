a499ed4d583d125b530d335484992fc8022ea40b
tsmsogn <tsmsogn@gmail.com>
2021-12-17 16:37:54 +0900

    価格変更に対応できるように

--- src/Calculator.php
+++ src/Calculator.php
@@ -18,10 +18,10 @@ class Calculator
      * @var int[] 決済手数料
      */
     protected $fees = [
-        9999 => 330,
-        29999 => 440,
-        99999 => 660,
-        300000 => 1100,
+        9999 => 300,
+        29999 => 400,
+        99999 => 600,
+        300000 => 1000,
     ];
 
     public function __construct($payment_type)
@@ -37,12 +37,13 @@ class Calculator
      * 入金額から代金引換手数料を求める
      *
      * @param $nyukin_price
+     * @param null $date 手数料計算日
      * @return int
      * @throws DaibikiLimitExceededException
      */
-    public function getFeeByNyukinPrice($nyukin_price)
+    public function getFeeByNyukinPrice($nyukin_price, $date = null)
     {
-        foreach ($this->getFees() as $threshold => $fee) {
+        foreach ($this->getFees($date) as $threshold => $fee) {
             $price = $nyukin_price + $fee;
             if ($price <= $threshold) {
                 return $fee;
@@ -53,10 +54,24 @@ class Calculator
     }
 
     /**
-     * @return int[]
+     * @param null $date 手数料計算日
+     * @return array|float[]|int[]
      */
-    public function getFees()
+    public function getFees($date = null)
     {
-        return $this->fees;
+        return array_map(function ($fee) use ($date) {
+            return $fee + $fee * $this->getTaxRate($date);
+        }, $this->fees);
+    }
+
+    /**
+     * 消費税率を返す
+     *
+     * @param null $date
+     * @return int
+     */
+    private function getTaxRate($date = null)
+    {
+        return 0.1;
     }
 }
